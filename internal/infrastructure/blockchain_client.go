6368c305e91399855a9b743715c0c9774bbac3bc
edutomesco <eduard.escoruela@gmail.com>
2021-11-09 13:57:48 +0100

    EIT-1623 | fixing http infra

--- internal/infrastructure/blockchain_client.go
+++ internal/infrastructure/blockchain_client.go
@@ -1,9 +1,7 @@
 package infrastructure
 
-import "github.com/enchainte/enchainte-sdk-go/config/entity"
-
 type BlockchainClient interface {
-	ValidateRoot(network entity.Network, root string) int
+	ValidateRoot(network string, root string) (int64, error)
 }
 
 
