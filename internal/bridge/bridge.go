35ebf6abbff5f08e17fca454317bdcda55550a70
marcbaque <marc@bloock.com>
2022-10-25 13:34:40 +0200

    Revert "clean"
    This reverts commit d2ddb3ec3a29147872e8e5523ec177a84c8e1b07.

---
+++ internal/bridge/bridge.go
@@ -0,0 +1,30 @@
+package bridge
+
+import "github.com/bloock/bloock-sdk-go/v2/internal/bridge/proto"
+
+type BloockBridge struct {
+	anchor proto.AnchorServiceClient
+	record proto.RecordServiceClient
+	proof  proto.ProofServiceClient
+}
+
+func NewBloockBridge() BloockBridge {
+	conn := NewBloockConnection()
+	return BloockBridge{
+		anchor: proto.NewAnchorServiceClient(conn),
+		record: proto.NewRecordServiceClient(conn),
+		proof:  proto.NewProofServiceClient(conn),
+	}
+}
+
+func (b *BloockBridge) Anchor() proto.AnchorServiceClient {
+	return b.anchor
+}
+
+func (b *BloockBridge) Record() proto.RecordServiceClient {
+	return b.record
+}
+
+func (b *BloockBridge) Proof() proto.ProofServiceClient {
+	return b.proof
+}
